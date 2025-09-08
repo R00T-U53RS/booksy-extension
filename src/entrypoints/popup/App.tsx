3e7ed2c8383cfda884f7f89d47cfe395f5e2a783
Z3RO-O <vaibhav.singh@fyle.in>
2025-09-08 14:44:21 +0530

    add shadcn and tailwind css

---
+++ src/entrypoints/popup/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const App = () => {
+  return (
+    <div className="flex items-center justify-center h-[600px] w-[400px]">
+      <Button>Click me</Button>
+    </div>
+  )
+}
+
+export default App;
--- wxt.config.ts
+++ wxt.config.ts
@@ -1,6 +1,21 @@
+import path from "path"
 import { defineConfig } from 'wxt';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  modules: ['@wxt-dev/module-react'],
+  srcDir: './src',
+  vite: () => ({
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  }),
+  manifest: {
+    name: 'Booksy',
+    description: 'Extension to manage and sync cross-browser bookmarks',
+  },
 });
